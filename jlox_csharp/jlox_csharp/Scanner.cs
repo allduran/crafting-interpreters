d2ec5c5b39ed7deee4a27eaa6084f6623b686d99
duran <cheshire.dd96@gmail.com>
2026-09-11 15:10:33 +0200

    Update .slnx and enhance Scanner for two-char tokens
    Updated solution file to set clox_c project ID and fix jlox_csharp.csproj path. In Scanner.cs, added support for '!', '=', '<', and '>' with single and double character tokens, introduced Match(char) helper, and added Microsoft.VisualBasic using directive.

--- jlox_csharp/jlox_csharp/Scanner.cs
+++ jlox_csharp/jlox_csharp/Scanner.cs
@@ -1,4 +1,6 @@
-﻿namespace JLox
+﻿using Microsoft.VisualBasic;
+
+namespace JLox
 {
     public class Scanner
     {
@@ -46,6 +48,18 @@
                 case '+': AddToken(TokenType.PLUS); break;
                 case ';': AddToken(TokenType.SEMICOLON); break;
                 case '*': AddToken(TokenType.STAR); break;
+                case '!': 
+                    AddToken(Match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);  
+                    break;
+                case '=':
+                    AddToken(Match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+                    break;
+                case '<':
+                    AddToken(Match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+                    break;
+                case '>':
+                    AddToken(Match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
+                    break;
                 default: 
                     Lox.Error(_line, $"Unexpected character: '{c}'"); 
                     break;
@@ -66,6 +80,15 @@
         {
             string text = _source.Substring(_start, _current - _start);
             _tokens.Add(new Token(type, text, literal, _line));
+        }
+
+        private bool Match(char expected)
+        {
+            if (IsAtEnd()) return false;
+            if (_source[_current] != expected) return false;
+
+            _current++;
+            return true;
         }
     }
 }
